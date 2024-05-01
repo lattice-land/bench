dffb234044df0f8993b30fd2092a821a38daf6b0
Pierre Talbot <pierre.talbot@uni.lu>
2024-05-01 11:27:40 +0200

    Improve again the benchmarking architecture.

--- benchmarks/benchmarking/minizinc/run.sh
+++ benchmarks/benchmarking/minizinc/run.sh
@@ -3,6 +3,9 @@
 # Exits when an error occurs.
 set -e
 
+BENCH_PATH=$(dirname $(realpath "$0"))
+BENCH_ROOT_PATH="$BENCH_PATH/../../"
+
 # I. Define the campaign to run.
 
 MZN_SOLVER="org.choco.choco"
@@ -10,38 +13,31 @@ VERSION="v4.10.13"
 MZN_TIMEOUT=1200000
 NUM_JOBS=8
 MACHINE="aion"
-INSTANCE_FILE="mzn2023.csv"
+INSTANCES_PATH="$BENCH_ROOT_PATH/benchmarking/mzn2023.csv"
 
 MZN_COMMAND="minizinc --solver $MZN_SOLVER -s --json-stream -t $MZN_TIMEOUT --output-mode json --output-time --output-objective"
-OUTPUT_DIR=$(pwd)"/../campaign/$MACHINE/$MZN_SOLVER-$VERSION"
+OUTPUT_DIR="$BENCH_ROOT_PATH/campaign/$MACHINE/$MZN_SOLVER-$VERSION"
 mkdir -p $OUTPUT_DIR
 
 ## II. Gather the list of Slurm nodes to run the experiments on many nodes if available.
 
 if [ -n "${SLURM_JOB_NODELIST}" ]; then
   # get host name
-  NODES_HOSTNAME="nodes_hostname.txt"
+  NODES_HOSTNAME="$BENCH_PATH/nodes_hostname.txt"
   scontrol show hostname $SLURM_JOB_NODELIST > $NODES_HOSTNAME
   # Collect public key and accept them
   while read -r node; do
       ssh-keyscan "$node" >> ~/.ssh/known_hosts
   done < "$NODES_HOSTNAME"
   MULTINODES_OPTION="--sshloginfile $NODES_HOSTNAME"
-
-  if [ -z "$1" ]; then
-    echo "Usage: $0 path_to_slurm_script (we store the slurm script in the campaign for replicability)"
-    exit 1
-  fi
-  cp "$1" $OUTPUT_DIR/ # this is the Slurm script
 fi
 
 # III. Run the experiments in parallel (one per sockets).
 
-DUMP_PY_PATH=$(pwd)/minizinc/dump.py
+DUMP_PY_PATH="$BENCH_PATH/dump.py"
 
 # For replicability.
-cp "$0" $OUTPUT_DIR/
-cp $DUMP_PY_PATH $OUTPUT_DIR/
-cp $INSTANCE_FILE $OUTPUT_DIR/
+cp -r $BENCH_PATH $OUTPUT_DIR/
+cp $INSTANCES_PATH $OUTPUT_DIR/$(basename "$BENCH_PATH")/
 
-parallel --no-run-if-empty $MULTINODES_OPTION --rpl '{} uq()' --jobs $NUM_JOBS -k --colsep ',' --skip-first-line $MZN_COMMAND {2} {3} '|' python3 $DUMP_PY_PATH $OUTPUT_DIR {1} {2} {3} $MZN_SOLVER :::: $INSTANCE_FILE
+parallel --no-run-if-empty $MULTINODES_OPTION --rpl '{} uq()' --jobs $NUM_JOBS -k --colsep ',' --skip-first-line $MZN_COMMAND {2} {3} '|' python3 $DUMP_PY_PATH $OUTPUT_DIR {1} {2} {3} $MZN_SOLVER :::: $INSTANCES_PATH
--- benchmarks/benchmarking/minizinc/slurm.sh
+++ benchmarks/benchmarking/minizinc/slurm.sh
@@ -2,12 +2,9 @@
 #SBATCH --time=03:00:00
 #SBATCH --nodes=10
 #SBATCH --partition=batch
-#SBATCH --account=p200244
-#SBATCH --qos=default
 #SBATCH --ntasks-per-node=8 # On Aion, we have 8 virtual processors per node.
 #SBATCH --ntasks-per-socket=1 # On Aion, a socket is a virtual processor.
 #SBATCH --cpus-per-task=8 # On Aion, each task has 8 cores available, although they should only use one.
-#SBATCH --cpu-bind=sockets # Request each task to be run on an independent socket (to try to avoid interferences).
 #SBATCH --export=ALL
 #SBATCH --output=slurm.out
 
@@ -19,4 +16,4 @@ if [ -z "$1" ]; then
     exit 1
 fi
 
-./"$1" "$0"
+./run.sh
