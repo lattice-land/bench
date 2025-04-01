80c23eec228e3c6b2056323303b51a4cbf70f191
Pierre Talbot <plgptalbot@login01.helios.cyfronet.pl>
2025-04-01 17:37:15 +0200

    Fix experiments for two problems.

--- benchmarks/campaign/helios/turbo.gpu.release-v1.2.8-mzn2024/turbo-mzn/run-helios.sh
+++ benchmarks/campaign/helios/turbo.gpu.release-v1.2.8-mzn2024/turbo-mzn/run-helios.sh
@@ -1,5 +1,5 @@
 #!/bin/bash -l
-#SBATCH --time=00:30:00
+#SBATCH --time=01:00:00
 #SBATCH -p plgrid-gpu-gh200
 #SBATCH -A plgturbo-gpu-gh200
 #SBATCH --gres=gpu:4
@@ -45,14 +45,14 @@ REAL_TIMEOUT=1200000
 ARCH="barebones"
 CORES=1 # The number of core used on the node.
 THREADS=0 # The number of core used on the node.
-FP="ac1"
-WAC1_THRESHOLD=4096
+FP="wac1"
+WAC1_THRESHOLD=0
 MACHINE=$(basename "$1" ".sh")
 INSTANCES_PATH="$BENCHMARKS_DIR_PATH/benchmarking/mzn2024_patch.csv"
 EXTRA_ARGS_TURBO=" -globalmem "
 
 # II. Prepare the command lines and output directory.
-MZN_COMMAND="minizinc --solver $MZN_SOLVER -s --json-stream -t $MZN_TIMEOUT --output-mode json --output-time --output-objective -p $THREADS -arch $ARCH -fp $FP -wac1_threshold $WAC1_THRESHOLD -hardware $MACHINE -version $VERSION -timeout $REAL_TIMEOUT $EXTRA_ARGS_TURBO "
+MZN_COMMAND="minizinc --solver $MZN_SOLVER -s --json-stream -t $MZN_TIMEOUT --fzn patch1.fzn --output-mode json --output-time --output-objective -p $THREADS -arch $ARCH -fp $FP -wac1_threshold $WAC1_THRESHOLD -hardware $MACHINE -version $VERSION -timeout $REAL_TIMEOUT $EXTRA_ARGS_TURBO "
 OUTPUT_DIR="$BENCHMARKS_DIR_PATH/campaign/$MACHINE/$MZN_SOLVER-$VERSION-mzn2024"
 mkdir -p $OUTPUT_DIR
 
