d0884ec0cee997adbaa5c01b974095bc2ae3b8ef
Pierre Talbot <plgptalbot@login01.helios.cyfronet.pl>
2025-05-15 07:09:18 +0200

    New experiments on 1.2.8.

--- benchmarks/benchmarking/turbo-mzn/run-helios.sh
+++ benchmarks/benchmarking/turbo-mzn/run-helios.sh
@@ -1,9 +1,9 @@
 #!/bin/bash -l
-#SBATCH --time=12:00:00
+#SBATCH --time=01:30:00
 #SBATCH -p plgrid-gpu-gh200
 #SBATCH -A plgturbo-gpu-gh200
 #SBATCH --gres=gpu:4
-#SBATCH --nodes=1
+#SBATCH --nodes=10
 #SBATCH -c 288
 #SBATCH --mem=0
 #SBATCH --qos=normal
