acb0f2cf43afa17fe3266535cda7e0bded4cd798
Pierre Talbot pierre.talbot@uni.lu <ptalbot@access1.iris-cluster.uni.lux>
2025-07-23 14:53:35 +0200

    Analysis of MZN 2024 instances.

--- benchmarks/campaign/aion/turbo.cpu.release-v1.2.9-mzn2024-analysis/turbo-mzn/run-iris-hybrid80.sh
+++ benchmarks/campaign/aion/turbo.cpu.release-v1.2.9-mzn2024-analysis/turbo-mzn/run-iris-hybrid80.sh
@@ -1,13 +1,14 @@
 #!/bin/bash -l
-#SBATCH --time=05:00:00
-#SBATCH --partition=batch
+#SBATCH --time=06:00:00
+#SBATCH --partition=gpu
 #SBATCH --nodes=1
+#SBATCH --gres=gpu:1
 #SBATCH --exclusive
-#SBATCH --ntasks-per-node=1
+#SBATCH --ntasks-per-node=1 # 
 #SBATCH --mem=0
 #SBATCH --qos=normal
 #SBATCH --export=ALL
-#SBATCH --output=slurm-turbo-analysis_cpu.out
+#SBATCH --output=slurm-turbo-hybrid80.out
 
 # Exits when an error occurs.
 set -e
@@ -25,7 +26,7 @@ if [ -z "$1" ]; then
   exit 1
 fi
 source $1
-source ${BENCHMARKS_DIR_PATH}/../pybench-aion/bin/activate
+source ${BENCHMARKS_DIR_PATH}/../pybench/bin/activate
 
 # If it has an argument, we retry the jobs that failed on a previous run.
 # If the experiments were not complete, you can simply rerun the script, parallel will ignore the jobs that are already done.
@@ -36,22 +37,25 @@ fi
 
 # I. Define the campaign to run.
 
-MZN_SOLVER="turbo.cpu.release"
-VERSION="v1.2.9" # Note that this is only for the naming of the output directory, we do not verify the actual version of the solver.
-ARCH="cpu"
-CORES=1 # The number of core used on the node.
-THREADS=1 # The number of core used on the node.
+MZN_SOLVER="turbo.gpu.release"
+VERSION="v1.2.4" # Note that this is only for the naming of the output directory, we do not verify the actual version of the solver.
+# This is to avoid MiniZinc to kill Turbo before it can print the statistics.
+MZN_TIMEOUT=360000
+REAL_TIMEOUT=300000
+ARCH="hybrid"
+CORES=28 # The number of core used on the node.
+THREADS=80 # The number of core used on the node.
 MACHINE=$(basename "$1" ".sh")
-INSTANCES_PATH="$BENCHMARKS_DIR_PATH/benchmarking/mzn2024-yumi-p9.csv"
+INSTANCES_PATH="$BENCHMARKS_DIR_PATH/benchmarking/short.csv"
 
 # II. Prepare the command lines and output directory.
-MZN_COMMAND="minizinc --solver $MZN_SOLVER -s --json-stream --output-mode json --output-time --output-objective -p $THREADS -arch $ARCH -network_analysis -cutnodes 1 -hardware $MACHINE -version $VERSION"
-OUTPUT_DIR="$BENCHMARKS_DIR_PATH/campaign/$MACHINE/$MZN_SOLVER-$VERSION-mzn2024-analysis"
+MZN_COMMAND="minizinc --solver $MZN_SOLVER -s --json-stream -t $MZN_TIMEOUT --output-mode json --output-time --output-objective -p $THREADS -arch $ARCH -hardware $MACHINE -version $VERSION -timeout $REAL_TIMEOUT"
+OUTPUT_DIR="$BENCHMARKS_DIR_PATH/campaign/$MACHINE/$MZN_SOLVER-$VERSION"
 mkdir -p $OUTPUT_DIR
 
 # If we are on the HPC, we encapsulate the command in a srun command to reserve the resources needed.
 if [ -n "${SLURM_JOB_NODELIST}" ]; then
-  SRUN_COMMAND="srun --exclusive --cpus-per-task=$CORES --nodes=1 --ntasks=1 --cpu-bind=verbose"
+  SRUN_COMMAND="srun --exclusive --cpus-per-task=$CORES --gpus-per-task=1 --nodes=1 --ntasks=1 --cpu-bind=verbose"
   NUM_PARALLEL_EXPERIMENTS=$((SLURM_JOB_NUM_NODES * 1)) # How many experiments are we running in parallel? One per GPU per default.
 else
   NUM_PARALLEL_EXPERIMENTS=1
@@ -70,4 +74,4 @@ lshw -json > $OUTPUT_DIR/$(basename "$MZN_WORKFLOW_PATH")/hardware-"$MACHINE".js
 # The `parallel` command spawns one `srun` command per experiment, which executes the minizinc solver with the right resources.
 
 COMMANDS_LOG="$OUTPUT_DIR/$(basename "$MZN_WORKFLOW_PATH")/jobs.log"
-parallel --verbose --no-run-if-empty --rpl '{} uq()' -k --colsep ',' --skip-first-line -j $NUM_PARALLEL_EXPERIMENTS --joblog $COMMANDS_LOG $SRUN_COMMAND $MZN_COMMAND $BENCHMARKING_DIR_PATH/{2} $BENCHMARKING_DIR_PATH/{3} '2>&1' '|' python3 $DUMP_PY_PATH $OUTPUT_DIR {1} {2} {3} $MZN_SOLVER $VERSION "0" $CORES $THREADS $ARCH "ac1" "0" :::: $INSTANCES_PATH
+parallel --verbose --no-run-if-empty --rpl '{} uq()' -k --colsep ',' --skip-first-line -j $NUM_PARALLEL_EXPERIMENTS --joblog $COMMANDS_LOG $SRUN_COMMAND $MZN_COMMAND $BENCHMARKING_DIR_PATH/{2} $BENCHMARKING_DIR_PATH/{3} '2>&1' '|' python3 $DUMP_PY_PATH $OUTPUT_DIR {1} {2} {3} $MZN_SOLVER $VERSION $REAL_TIMEOUT $CORES $THREADS $ARCH :::: $INSTANCES_PATH
