8cdf7768ce77670e2112b9593a0093d00642fff4
Pierre Talbot <pierre.talbot@uni.lu>
2025-11-15 08:25:26 +0100

    precision7750

---
+++ benchmarks/campaign/precision7750/turbo.gpu.release-v1.2.8-mzn2024/turbo-mzn/helios.sh
@@ -0,0 +1,9 @@
+#!/bin/bash -l
+
+module load GCCcore/14.3.0
+module load parallel/20250922
+module load CUDA/12.9.1
+module load CMake/3.27.6
+module load Doxygen/1.9.8
+module load libxml2/2.11.5
+module load Python/3.11.5
