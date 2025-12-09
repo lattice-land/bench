41a9033f8453a43c9c60dfd611c3a58d07180913
Pierre Talbot <pierre.talbot@uni.lu>
2025-12-09 09:00:34 +0100

    bad xps

--- benchmarks/campaign/iris-hopper/turbo.gpu.release-v1.2.8-mzn2024/turbo-mzn/aion.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash -l
-
-module use "/work/projects/software_set/easybuild/${ULHPC_CLUSTER}/2023b/${RESIF_ARCH}/modules/all" # Use the latest software chain of AION.
-module load devel/CMake
-module load devel/Doxygen
-module load lib/libxml2
-module load lang/Python
--- benchmarks/campaign/iris-hopper/turbo.gpu.release-v1.2.8-mzn2024/turbo-mzn/iris-hopper.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash -l
-
-#module use "/work/projects/software_set/easybuild/${ULHPC_CLUSTER}/2023b/${RESIF_ARCH}/modules/all" # Use the latest software chain of IRIS.
-module load system/CUDA
-module load devel/CMake
-module load devel/Doxygen
-module load lib/libxml2
-module load lang/Python
